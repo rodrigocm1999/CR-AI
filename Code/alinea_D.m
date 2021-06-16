cf9af1776befcb7309560213e9a73cdacca8dad9
Rodrigo Cavadas Mendes <rodrigocm1999@gmail.com>
2021-06-16 15:38:57 +0100

    asd

--- Code/alinea_D.m
+++ Code/alinea_D.m
@@ -3,21 +3,19 @@ networkId = '13';
 load([networkFolder networkId '.mat']);
 
 imgResolution = 12;
-
 [manualInputs, manualTargets] = readyImages('Manual', imgResolution, '%d','jpg');
 
 imageOutputs = sim(net, manualInputs);
 
 
-amountOfElements = size(imageOutputs,2);
 correctOutput=0;
-for i=1:amountOfElements                        % Para cada classificacao
-    [~, obtained] = max(imageOutputs(:, i));    % guarda a linha onde encontrou valor mais alto da saida obtida
-    [~, supposed] = max(manualTargets(:, i));   % guarda a linha onde encontrou valor mais alto da saida desejada
-    if obtained == supposed                     % se estao na mesma linha, a classificacao foi correta (incrementa 1)
+amountOfElements = size(imageOutputs,2);
+for i=1:amountOfElements
+    [~, obtained] = max(imageOutputs(:, i));
+    [~, supposed] = max(manualTargets(:, i));
+    if obtained == supposed
         correctOutput = correctOutput + 1;
     else
-        imageOutputs(:,i)
         fprintf('Imagem não deu certo (i) -> %d, invés deu -> %d\n', supposed, obtained)
     end
 end
